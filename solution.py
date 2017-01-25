580052b13e13c988eca591aec3d6665e0c6a6b70
Donnie Adams <donald.g.adams@me.com>
2017-01-25 12:05:20 -0700

    Passing the test cases

--- solution.py
+++ solution.py
@@ -1,5 +1,14 @@
 assignments = []
 
+
+def update_units_for_diagonal():
+    UNITLIST.extend(DIAGONAL_UNITS)
+    for diagonal in DIAGONAL_UNITS:
+        for box in diagonal:
+            UNITS[box].append(diagonal)
+            PEERS[box].update(set(diagonal) - set([box]))
+
+
 def assign_value(values, box, value):
     """
     Please use this function to update your values dictionary!
@@ -10,8 +19,10 @@ def assign_value(values, box, value):
         assignments.append(values.copy())
     return values
 
+
 def naked_twins(values):
     """Eliminate values using the naked twins strategy.
+
     Args:
         values(dict): a dictionary of the form {'box_name': '123456789', ...}
 
@@ -21,10 +32,25 @@ def naked_twins(values):
 
     # Find all instances of naked twins
     # Eliminate the naked twins as possibilities for their peers
+    twins = []
+    for box in BOXES:
+        if len(values[box]) == 2:
+            for other_box in PEERS[box]:
+                if values[other_box] == values[box]:
+                    twins.append([box, other_box])
+    for box1, box2 in twins:
+        if len(values[box1]) < 2 or len(values[box2]) < 2:
+            continue
+        for box in PEERS[box1] & PEERS[box2]:
+            if len(values[box]) > 1:
+                values[box] = values[box].replace(values[box1][0], "").replace(values[box1][1], "")
+    return values
+
 
 def cross(A, B):
     "Cross product of elements in A and elements in B."
-    pass
+    return [a + b for a in A for b in B]
+
 
 def grid_values(grid):
     """
@@ -36,7 +62,13 @@ def grid_values(grid):
             Keys: The boxes, e.g., 'A1'
             Values: The value in each box, e.g., '8'. If the box has no value, then the value will be '123456789'.
     """
-    pass
+    values = dict()
+    index = 0
+    while index < len(BOXES):
+        values[BOXES[index]] = grid[index] if grid[index] != '.' else COLS
+        index += 1
+    return values
+
 
 def display(values):
     """
@@ -44,29 +76,93 @@ def display(values):
     Args:
         values(dict): The sudoku in dictionary form
     """
-    pass
+    width = 1 + max(len(values[s]) for s in values.keys())
+    line = '+'.join(['-' * (width * 3)] * 3)
+    for row in ROWS:
+        print(''.join(values[row + col].center(width) + ('|' if col in '36' else '') for col in COLS))
+        if row in 'CF':
+            print(line)
+    print()
+
 
 def eliminate(values):
-    pass
+    only_one_value = [box for box in BOXES if len(values[box]) == 1]
+    for box in only_one_value:
+        for other_box in PEERS[box]:
+            values[other_box] = values[other_box].replace(values[box], "")
+    return values
+
 
 def only_choice(values):
-    pass
+    for unit in UNITLIST:
+        for val in COLS:
+            box_with_val = [box for box in unit if val in values[box]]
+            if len(box_with_val) == 1:
+                values[box_with_val[0]] = val
+    return values
+
 
 def reduce_puzzle(values):
-    pass
+    stalled = False
+    while not stalled:
+        num_solved_before = len([box for box in values if len(values[box]) == 1])
+        values = eliminate(values)
+        values = only_choice(values)
+        values = naked_twins(values)
+        if any([len(val) == 0 for val in values.values()]):
+            return None
+        num_solved_after = len([box for box in values if len(values[box]) == 1])
+        stalled = num_solved_before == num_solved_after
+    return values
+
+
+def solve(grid, diagonal=True):
+    """
+     Find the solution to a Sudoku grid.
+     Args:
+         grid(string): a string representing a sudoku grid.
+             Example: '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
+     Returns:
+         The string representing the final sudoku grid. False if no solution exists.
+     """
+    values = grid_values(grid)
+    if diagonal:
+        update_units_for_diagonal()
+    return search(values)
+
 
 def search(values):
-    pass
+    values = reduce_puzzle(values)
+    if values is None:
+        return None
+    if all([len(values[b]) == 1 for b in values.keys()]):
+        return values
+    min_values = 10
+    next_box = ""
+    for b, vals in values.items():
+        if len(vals) > 1 and len(vals) < min_values:
+            min_values = len(vals)
+            next_box = b
 
-def solve(grid):
-    """
-    Find the solution to a Sudoku grid.
-    Args:
-        grid(string): a string representing a sudoku grid.
-            Example: '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
-    Returns:
-        The string representing the final sudoku grid. False if no solution exists.
-    """
+    for value in values[next_box]:
+        new_values = values.copy()
+        new_values[next_box] = value
+        possible_sol = search(new_values)
+        if possible_sol is not None:
+            return possible_sol
+
+# Copying these utilities from the course because order matters to pass the test cases.
+ROWS = 'ABCDEFGHI'
+COLS = '123456789'
+BOXES = cross(ROWS, COLS)
+ROW_UNITS = [cross(r, COLS) for r in ROWS]
+COL_UNITS = [cross(ROWS, c) for c in COLS]
+SQUARE_UNITS = [cross(rs, cs) for rs in ('ABC', 'DEF', 'GHI') for cs in ('123', '456', '789')]
+DIAGONAL_UNITS = [["".join(a) for a in zip(ROWS, COLS)], ["".join(a) for a in zip(ROWS, reversed(COLS))]]
+UNITLIST = ROW_UNITS + COL_UNITS + SQUARE_UNITS
+UNITS = dict((s, [u for u in UNITLIST if s in u]) for s in BOXES)
+PEERS = dict((s, set(sum(UNITS[s], [])) - set([s])) for s in BOXES)
+
 
 if __name__ == '__main__':
     diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
